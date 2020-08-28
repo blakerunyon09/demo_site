961df6b0cb3577bdd7c5bb60adfcfe8e4c9b7587
Blake Runyon <blakerunyon@Blakes-MacBook-Pro.local>
2020-08-27 18:58:43 -0600

    Updates to to-do project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-document.write("Your JS is WORKING");
+// Selectors
+const todoForm = document.querySelector("#todo-form");
+let todoInput = todoForm.childNodes[1];
+const todoSubmit = todoForm.childNodes[3];
+const fullList = document.querySelector("#todo-list");
+
+// Listeners
+todoSubmit.addEventListener("click", addListItem);
+
+// Functions
+function addListItem(e) {
+  e.preventDefault();
+  console.log(todoInput.value);
+  fullList.innerHTML += `<li>${todoInput.value}`;
+  fullList.innerHTML += `<span class="done">Done</span>`;
+  fullList.innerHTML += `<span class="delete">Delete<span></li>`;
+  todoInput.value = "";
+}
