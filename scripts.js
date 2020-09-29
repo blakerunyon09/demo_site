91a106d5146fbd00d631708ef083c4e79c346aeb
Blake Runyon <blakerunyon@Blakes-MacBook-Pro.local>
2020-09-29 13:50:05 -0600

    Verison 1.0 Ready

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@
 const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.childNodes[1];
 const todoSubmit = todoForm.childNodes[2];
+const todoFilter = todoForm.childNodes[4];
 const fullList = document.querySelector("#todo-list");
 
 // Listeners
 todoSubmit.addEventListener("click", addListItem);
 fullList.addEventListener("click", updateItems);
+todoFilter.addEventListener("change", filterItems);
 
 // Functions
 function addListItem(e) {
@@ -32,11 +34,38 @@ function addListItem(e) {
 
 function updateItems(e) {
   const item = e.target;
-  if (item.innerText === "Delete") {
+  if (item.className === "fas fa-times") {
     //Delete Items
     item.parentNode.remove();
-  } else if (item.innerText === "Done") {
-    // Cross Out Items
+  } else if (item.className === "far fa-circle") {
     item.parentNode.style.textDecoration = "line-through";
+    item.className = "fas fa-check-circle";
+  } else if (item.className === "fas fa-check-circle") {
+    item.parentNode.style.textDecoration = "none";
+    item.className = "far fa-circle";
+  }
+}
+
+function filterItems() {
+  if (todoFilter.selectedIndex === 1) {
+    for (let i = 0; i < fullList.children.length; i++) {
+      if (fullList.children[i].style.textDecoration !== "line-through") {
+        fullList.children[i].style.display = "none";
+      } else {
+        fullList.children[i].style.display = "list-item";
+      }
+    }
+  } else if (todoFilter.selectedIndex === 2) {
+    for (let i = 0; i < fullList.children.length; i++) {
+      if (fullList.children[i].style.textDecoration === "") {
+        fullList.children[i].style.display = "list-item";
+      } else {
+        fullList.children[i].style.display = "none";
+      }
+    }
+  } else {
+    for (let i = 0; i < fullList.children.length; i++) {
+      fullList.children[i].style.display = "list-item";
+    }
   }
 }
